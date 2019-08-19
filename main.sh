94b6fdaa80e35557662914027eb0ae43e0621ce0
LeviHarrison <54278938+LeviHarrison@users.noreply.github.com>
2019-08-19 10:40:26 -0400

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,30 @@
+echo -e "\e[32mHack Initiating...";
+sleep 5
+for i in {1..25}
+do
+echo -e "\e[32mhawuioehfoauwheffwaeu";
+sleep .02;
+echo -e "\e[32mihwfheuwhfiwhfhfwuihfiwuhfufiwh";
+sleep .02;
+echo -e "\e[32meiifj";
+sleep .02;
+echo -e "\e[32mwiofiejowjfiojfifjojifijfojfoijfowifjwojfpaeijaje8ifiojfiefjifjojf";
+sleep .02;
+echo -e "\e[32mepjfsfjesjfipejpsijfipjfspijfpsjfpsjfipjfz;j[azijfw0jf0wjfwepifjwfi ew0fj";
+sleep .02;
+echo -e "\e[32mwifjwpifjwpfji";
+sleep .02;
+echo -e "\e[123837wq8df8d0q7d";
+sleep .02;
+echo -e "\e[141fd-f82n-r18fyn1-cyrn1-cy83rcn-18rycn-38yc1nc-";
+sleep .02;
+echo -e "\e[321239180493901-4908";
+sleep .02;
+echo -e "\e[84327408979213749812742794nnc398cn78100cvv41n0cn1cn03147c1n40c7n120947cn01cn";
+sleep .02;
+echo -e "\e[32m908347c0871n09nv9n9817v9874n1327947v12034v7n10n0123v7n048n3109247n19032479407132094712904712n419047nv812904719024719247129047";
+sleep .02;
+echo -e "\e[1";
+sleep .02;
+done
+echo -e "\e[39m"
